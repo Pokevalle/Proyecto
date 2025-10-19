ff30adaca98ea7227ef315b6a796afc815f8b94a
unknown <pokevalle360@gmail.com>
2025-10-18 23:33:52 -0600

    Primer Commit

---
+++ ProyectoFinal/src/proyectofinal/ProyectoFinal.java
@@ -0,0 +1,17 @@
+package proyectofinal;
+
+/**
+ *
+ * @author SlayerX
+ */
+public class ProyectoFinal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       dbConection dbc = new dbConection();
+       dbc.conectar();
+    }
+    
+}
